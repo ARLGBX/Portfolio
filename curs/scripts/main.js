3e06c05b6545c77bd5374102031db8c536193b87
gabdy <mpizza528@gmail.com>
2025-03-27 22:27:51 +0500

    Сделал много еще и немного много и еще и еще и еще и еще и оооооооочень много и немного и еще и еще И ЕШЕ b tot и еше немного и еще и еще и еще

---
+++ curs/scripts/main.js
@@ -0,0 +1,10 @@
+new Swiper('.workers__sliders', {
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    }
+});
